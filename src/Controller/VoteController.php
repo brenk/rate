46d228766db81d405e6a59b4b5048f1af7a57d36
William Renk <william.renk@workday.com>
2016-08-04 07:03:29 -0700

    refactored bot detection to service

--- src/Controller/VoteController.php
+++ src/Controller/VoteController.php
@@ -6,6 +6,7 @@ use Drupal\Core\Ajax\ReplaceCommand;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\rate\RateBotDetector;
 use Drupal\votingapi\Entity\Vote;
 use Drupal\node\Entity\Node;
 use Drupal\rate\RateEntityVoteTotals;
@@ -17,180 +18,6 @@ use Symfony\Component\HttpFoundation\Request;
  * Returns responses for Rate routes.
  */
 class VoteController extends ControllerBase implements ContainerInjectionInterface {
-
-  /**
-   * Check if the given IP is a local IP-address.
-   *
-   * @param string $ip
-   *   IP address to check.
-   *
-   * @return bool
-   *   True if local IP; false otherwise.
-   */
-  private function botsIsLocal($ip) {
-    if (preg_match('/^([012]?[0-9]{2})\\./', $ip, $match)) {
-      switch ($match[1]) {
-        case 10:
-        case 127:
-        case 172:
-        case 192:
-          return TRUE;
-      }
-    }
-    return FALSE;
-  }
-
-  /**
-   * Save IP address as a bot.
-   *
-   * @param string $ip
-   *   IP address to register as bot.
-   */
-  private function botsRegisterBot($ip) {
-    // @Todo: update this.
-    db_insert('rate_bot_ip')->fields(array('ip' => $ip))->execute();
-  }
-
-  /**
-   * Check if the IP-address exists in the local bot database.
-   *
-   * @param string $ip
-   *   IP Address to check.
-   *
-   * @return bool
-   *   TRUE if IP is in database; false otherwise.
-   */
-  private function botsCheckIp($ip) {
-    // @Todo: update this.
-    return (bool) db_select('rate_bot_ip', 'rbi')
-      ->fields('rbi', array('id'))
-      ->condition('rbi.ip', $ip)
-      ->range(0, 1)
-      ->execute()
-      ->fetchField();
-  }
-
-  /**
-   * Check if the given user agent matches the local bot database.
-   *
-   * @param string $agent
-   *   User agent to check.
-   *
-   * @return bool
-   *   True if match found; false otherwise.
-   */
-  private function botsCheckAgent($agent) {
-    $sql = 'SELECT 1 FROM {rate_bot_agent} WHERE :agent LIKE pattern LIMIT 1';
-    // @Todo: update this.
-    return (bool) db_query($sql, array(':agent' => $agent))->fetchField();
-  }
-
-  /**
-   * Check the number of votes between now and $interval seconds ago.
-   *
-   * @param string $ip
-   *   IP address to check.
-   * @param int $interval
-   *   Interval in seconds.
-   *
-   * @return int
-   *   Number of votes between not and internval.
-   */
-  private function botsCheckThreshold($ip, $interval) {
-    $sql = 'SELECT COUNT(*) FROM {votingapi_vote} WHERE vote_source = :ip AND timestamp > :time';
-    return db_query($sql, array(':ip' => $ip, ':time' => REQUEST_TIME - $interval))->fetchField();
-  }
-
-  /**
-   * Check if botscout thinks the IP is a bot.
-   *
-   * @param string $ip
-   *   IP to check.
-   *
-   * @return bool
-   *   True if botscout returns a positive; false otherwise.
-   */
-  private function botsCheckBotscout($ip) {
-    $config = \Drupal::config('rate.settings');
-    $key = $config->get('botscout_key');
-
-    if ($key) {
-      $uri = "http://botscout.com/test/?ip=$ip&key=$key";
-
-      try {
-        $response = \Drupal::httpClient()->get($uri, array('headers' => array('Accept' => 'text/plain')));
-        $data = (string) $response->getBody();
-        $status_code = $response->getStatusCode();
-        if (!empty($data) && $status_code == 200) {
-          if ($data{0} == 'Y') {
-            return TRUE;
-          }
-        }
-      }
-      catch (RequestException $e) {
-        drupal_set_message(t('An error occurred contacting BotScout.'), 'warning');
-        watchdog_exception('rate', $e);
-      }
-    }
-
-    return FALSE;
-  }
-
-  /**
-   * Check if the current user is blocked.
-   *
-   * This function will first check if the user is already known to be a bot.
-   * If not, it will check if we have valid reasons to assume the user is a bot.
-   *
-   * @return bool
-   *   True if bot detected; false otherwise.
-   */
-  private function botsIsBot() {
-    $ip = \Drupal::request()->getClientIp();
-    $agent = $_SERVER['HTTP_USER_AGENT'];
-
-    if ($this->botsIsLocal($ip)) {
-      // The IP-address is a local IP-address. This is probably because of
-      // misconfigured proxy servers. Do only the user agent check.
-      return $this->botsCheckAgent($agent);
-    }
-
-    if ($this->botsCheckIp($ip)) {
-      return TRUE;
-    }
-
-    if ($this->botsCheckAgent($agent)) {
-      // Identified as a bot by its user agent. Register this bot by IP-address
-      // as well, in case this bots uses multiple agent strings.
-      $this->botsRegisterBot($ip);
-      return TRUE;
-    }
-
-    $config = \Drupal::config('rate.settings');
-    $threshold = $config->get('bot_minute_threshold', 25);
-
-    if ($threshold && ($this->botsCheckThreshold($ip, 60) > $threshold)) {
-      $this->botsRegisterBot($ip);
-      return TRUE;
-    }
-
-    $threshold = $config->get('bot_hour_threshold', 250);
-
-    // Always count, even if threshold is disabled. This is to determine if we
-    // can skip the BotScout check.
-    $count = $this->botsCheckThreshold($ip, 3600);
-    if ($threshold && ($count > $threshold)) {
-      $this->botsRegisterBot($ip);
-      return TRUE;
-    }
-
-    if (!$count && $this->botsCheckBotscout($ip)) {
-      $this->botsRegisterBot($ip);
-      return TRUE;
-    }
-
-    return FALSE;
-  }
 
   /**
    * Record a vote.
@@ -208,7 +35,9 @@ class VoteController extends ControllerBase implements ContainerInjectionInterfa
    *   Redirect to path provided in request.
    */
   public function vote($entity_type_id, $vote_type_id, $entity_id, Request $request) {
-    $is_bot_vote = $this->botsIsBot();
+    $databae = \Drupal::database();
+    $bot_detector = new RateBotDetector($databae);
+    $is_bot_vote = $bot_detector->checkIsBot();
     $config = \Drupal::config('rate.settings');
     $use_ajax = $config->get('use_ajax', FALSE);
 
---
+++ src/RateBotDetector.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace Drupal\rate;
+
+use GuzzleHttp\Exception\RequestException;
+use Drupal\Core\Database\Connection;
+
+/**
+ * The rate.entity_vote_totals service.
+ */
+class RateBotDetector {
+
+  /**
+   * Client IP.
+   *
+   * @var string
+   */
+  protected $ip;
+
+  /**
+   * HTTP User agent.
+   *
+   * @var string
+   */
+  protected $agent;
+
+  /**
+   * Rate settings object.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $rate_settings;
+
+  /**
+   * Database connection object.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * RateBotDetector constructor.
+   *
+   * @param Connection $database
+   *   Database connection object.
+   */
+  public function __construct(Connection $database) {
+    $this->ip = \Drupal::request()->getClientIp();
+    $this->agent = $_SERVER['HTTP_USER_AGENT'];
+    $this->rate_settings = \Drupal::config('rate.settings');
+    $this->database = $database;
+  }
+
+  /**
+   * Check if the given IP is a local IP-address.
+   *
+   * @return bool
+   *   True if local IP; false otherwise.
+   */
+  private function isLocal() {
+    if (preg_match('/^([012]?[0-9]{2})\\./', $this->ip, $match)) {
+      switch ($match[1]) {
+        case 10:
+        case 127:
+        case 172:
+        case 192:
+          return TRUE;
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * Save IP address as a bot.
+   */
+  private function registerBot() {
+    $this->database->insert('rate_bot_ip')->fields(array('ip' => $this->ip))->execute();
+  }
+
+  /**
+   * Check if the IP-address exists in the local bot database.
+   *
+   * @return bool
+   *   TRUE if IP is in database; false otherwise.
+   */
+  protected function checkIp() {
+    return (bool) $this->database->select('rate_bot_ip', 'rbi')
+      ->fields('rbi', array('id'))
+      ->condition('rbi.ip', $this->ip)
+      ->range(0, 1)
+      ->execute()
+      ->fetchField();
+  }
+
+  /**
+   * Check if the given user agent matches the local bot database.
+   *
+   * @return bool
+   *   True if match found; false otherwise.
+   */
+  protected function checkAgent() {
+    $sql = 'SELECT 1 FROM {rate_bot_agent} WHERE :agent LIKE pattern LIMIT 1';
+    return (bool) $this->database->query($sql, array(':agent' => $this->agent))->fetchField();
+  }
+
+  /**
+   * Check the number of votes between now and $interval seconds ago.
+   *
+   * @param int $interval
+   *   Interval in seconds.
+   *
+   * @return int
+   *   Number of votes between not and internval.
+   */
+  protected function checkThreshold($interval) {
+    $sql = 'SELECT COUNT(*) FROM {votingapi_vote} WHERE vote_source = :ip AND timestamp > :time';
+    return $this->database->query($sql, array(':ip' => $this->ip, ':time' => REQUEST_TIME - $interval))->fetchField();
+  }
+
+  /**
+   * Check if botscout thinks the IP is a bot.
+   *
+   * @return bool
+   *   True if botscout returns a positive; false otherwise.
+   */
+  protected function checkBotscout() {
+    $key = $this->rate_settings->get('botscout_key');
+
+    if ($key) {
+      $uri = "http://botscout.com/test/?ip=$this->ip&key=$key";
+
+      try {
+        $response = \Drupal::httpClient()->get($uri, array('headers' => array('Accept' => 'text/plain')));
+        $data = (string) $response->getBody();
+        $status_code = $response->getStatusCode();
+        if (!empty($data) && $status_code == 200) {
+          if ($data{0} == 'Y') {
+            return TRUE;
+          }
+        }
+      }
+      catch (RequestException $e) {
+        drupal_set_message(t('An error occurred contacting BotScout.'), 'warning');
+        watchdog_exception('rate', $e);
+      }
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Check if the current user is blocked.
+   *
+   * This function will first check if the user is already known to be a bot.
+   * If not, it will check if we have valid reasons to assume the user is a bot.
+   *
+   * @return bool
+   *   True if bot detected; false otherwise.
+   */
+  public function checkIsBot() {
+    if ($this->isLocal()) {
+      // The IP-address is a local IP-address. This is probably because of
+      // misconfigured proxy servers. Do only the user agent check.
+      return $this->checkAgent();
+    }
+
+    if ($this->checkIp()) {
+      return TRUE;
+    }
+
+    if ($this->checkAgent()) {
+      // Identified as a bot by its user agent. Register this bot by IP-address
+      // as well, in case this bots uses multiple agent strings.
+      $this->registerBot();
+      return TRUE;
+    }
+
+    $threshold = $this->rate_settings->get('bot_minute_threshold');
+
+    if ($threshold && ($this->checkThreshold(60) > $threshold)) {
+      $this->registerBot();
+      return TRUE;
+    }
+
+    $threshold = $this->rate_settings->get('bot_hour_threshold');
+
+    // Always count, even if threshold is disabled. This is to determine if we
+    // can skip the BotScout check.
+    $count = $this->checkThreshold(3600);
+    if ($threshold && ($count > $threshold)) {
+      $this->registerBot();
+      return TRUE;
+    }
+
+    if (!$count && $this->checkBotscout()) {
+      $this->registerBot();
+      return TRUE;
+    }
+
+    return FALSE;
+  }
+
+}
